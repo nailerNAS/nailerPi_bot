2593ee7bc5c077ec012801a3a08af8b2eb4a3cee
nailer <nailer@protonmail.ch>
2018-02-24 15:12:03 +0200

    autopep8 formatting

--- remotes.py
+++ remotes.py
@@ -17,48 +17,44 @@ class NailerPI:
         self.mac = mac
         self.eth0 = eth0
         self.wlan0 = wlan0
-    
 
     def online(self):
         msg = '_Reloeded!_\n\n\n{}'.format(self.ifconfig())
         return msg
 
-
     def run(self, cmd):
         try:
-            proc = subprocess.Popen(cmd, executable='/bin/bash', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+            proc = subprocess.Popen(cmd, executable='/bin/bash', shell=True,
+                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
             return str(proc.stdout.read())
         except Exception as e:
             return str(e)
-    
 
     def shutdown(self):
         return self.run('sudo poweroff')
-    
+
     def reboot(self):
         return self.run('sudo reboot')
-    
+
     def cpu_temp(self):
         try:
             with open('/sys/class/thermal/thermal_zone0/temp', 'rt') as file:
                 tmp = file.read()
-            tmp = int(tmp) / 1000 # getting the right value
+            tmp = int(tmp) / 1000  # getting the right value
             tmp = round(tmp, 2)   # rouding it to two digits (optional)
 
             return str(tmp)
         except Exception as e:
             return str(e)
-    
 
     def ethernet(self):
         return self.run('/sbin/ifconfig {}'.format(self.eth0))
-    
+
     def wireless(self):
         return self.run('/sbin/ifconfig {}'.format(self.wlan0))
-    
+
     def ifconfig(self):
         return self.run('/sbin/ifconfig')
-
 
     def wol(self):
         return self.run('/sbin/wakeonlan {}'.format(self.mac))
